4092c8cff15947c0d92ee94bf3619a3337bd1122
rishard <info@masterpro.ws>
2022-08-27 02:23:15 +0300

    the link in the footer has been changed

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer">
-    <p className="link-action">OpenWeatherMap API + Geolocation API + ReactJS. <a href="https://masterpro.ws" title="Masterpro WebLaboratory" target="_blank" rel="noreferrer">Masterpro.ws</a> © 2009 - 2022</p>
+    <p className="link-action">OpenWeatherMap API + Geolocation API + ReactJS. <a href="https://masterpro.ws/api-openweathermap-w3c-geolocation-api-reactjs" title="Masterpro Web Laboratory" target="_blank" rel="noreferrer">Masterpro.ws</a> © 2009 - 2022</p>
   </div>
 );
 
