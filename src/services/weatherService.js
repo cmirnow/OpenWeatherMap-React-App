1e70935ebd9ab32c6834a3609a54cbf642950eb3
rishard <info@masterpro.ws>
2025-03-09 10:45:19 +0300

    unique cache key for each coordinate pair

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,16 +1,21 @@
 const cache = {
-  current: { data: null, timestamp: 0 },
-  forecast: { data: null, timestamp: 0 },
+  current: {}, // Cache for current weather: key - string “lat, lon”, value - { data, timestamp }
+  forecast: {}, // Forecast cache: key - string “lat, lon”, value - { data, timestamp }
 };
 
 const CACHE_TTL = 60 * 60 * 1000; // 1 hour milliseconds
 
-export const fetchWeatherData = async (url, setData, cacheKey) => {
+export const fetchWeatherData = async (url, setData, cacheType, lat, lon) => {
+  const cacheKey = `${lat},${lon}`; // Unique key for each coordinate pair
   const now = Date.now();
 
   // If there is data in the cache and it is not older than 1 hour, we use it
-  if (cache[cacheKey].data && now - cache[cacheKey].timestamp < CACHE_TTL) {
-    setData(cache[cacheKey].data);
+  if (
+    cache[cacheType][cacheKey] &&
+    cache[cacheType][cacheKey].data &&
+    now - cache[cacheType][cacheKey].timestamp < CACHE_TTL
+  ) {
+    setData(cache[cacheType][cacheKey].data);
     return;
   }
 
@@ -21,8 +26,10 @@ export const fetchWeatherData = async (url, setData, cacheKey) => {
     }
     const data = await response.json();
     // Save data to cache with the current timestamp
-    cache[cacheKey].data = data;
-    cache[cacheKey].timestamp = now;
+    cache[cacheType][cacheKey] = {
+      data,
+      timestamp: now,
+    };
     setData(data);
   } catch (error) {
     throw error;
@@ -31,10 +38,10 @@ export const fetchWeatherData = async (url, setData, cacheKey) => {
 
 export const fetchCurrentWeather = (lat, lon, setApiData) => {
   const url = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`;
-  return fetchWeatherData(url, setApiData, "current");
+  return fetchWeatherData(url, setApiData, "current", lat, lon);
 };
 
 export const fetchForecastWeather = (lat, lon, setApiDataForecast) => {
   const url = `//api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=16&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`;
-  return fetchWeatherData(url, setApiDataForecast, "forecast");
+  return fetchWeatherData(url, setApiDataForecast, "forecast", lat, lon);
 };
